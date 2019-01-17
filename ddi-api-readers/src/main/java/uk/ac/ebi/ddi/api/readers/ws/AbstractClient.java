83c11a8e701be17fe5bdbf072cdf8e2d95df0301
Tu Manh Vu <glmanhtu@gmail.com>
2019-01-17 15:53:59 +0000

    Increase time out because of API http://www.metabolomicsworkbench.org/rest/study/study_id/ST/summary too slow

--- ddi-api-readers/src/main/java/uk/ac/ebi/ddi/api/readers/ws/AbstractClient.java
+++ ddi-api-readers/src/main/java/uk/ac/ebi/ddi/api/readers/ws/AbstractClient.java
@@ -3,13 +3,14 @@ package uk.ac.ebi.ddi.api.readers.ws;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import uk.ac.ebi.ddi.api.readers.utils.RetryClient;
 
 /**
  * Abstract client to query the EBI search.
  *
  * @author ypriverol
  */
-public abstract class AbstractClient {
+public abstract class AbstractClient extends RetryClient {
 
     protected RestTemplate restTemplate;
 
@@ -30,7 +31,7 @@ public abstract class AbstractClient {
      */
     private ClientHttpRequestFactory clientHttpRequestFactory() {
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        int timeOut = 20000;
+        int timeOut = 200000;
         factory.setReadTimeout(timeOut);
         factory.setConnectTimeout(timeOut);
         return factory;
